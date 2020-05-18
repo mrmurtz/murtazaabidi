7bdc45bdac4c6680d2680ccc494c26d7ce54eded
mrmurtz <murtaza.s.abidi@gmail.com>
2020-05-18 20:03:35 +0100

    hero section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo1 from './logo.svg';
+import logo from './images/logo.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav>
+        <a href="" className='logo'>✌🏽 Murtaza Abidi</a>
+        <div className="navigation-items">
+          <a href="" className="item">About</a>
+          <a href="" className="item">Projects</a>
+        </div>
+      </nav>
+
+      <section>
+        <div className="hero">
+          <h1>{"Hi, I'm Murtaza."}</h1>
+          <h2>{"I'm a product designer at"} <a href="https://transferwise.com" className="highlight">TransferWise</a>. {"I design things to help solve problems for people. Sometimes I write about things I care about. And I shout about things on twitter too"}.</h2>
+        </div>
+      </section>
+
+      <footer>
+        <a href="https://twitter.com/murtz_abidi">Twitter</a>
+      </footer>
     </div>
   );
 }
