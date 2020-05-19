04debb354231065ec34227e679f8e26df15df27d
mrmurtz <murtaza.s.abidi@gmail.com>
2020-05-19 19:08:45 +0100

    New layout and breakpoints

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
 import React from 'react';
 import logo1 from './logo.svg';
 import logo from './images/logo.svg'
+import arrow1 from './images/arrow1.svg'
+import arrow2 from './images/arrow2.svg'
+import murtaza from './images/murtaza.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <nav>
-        <a href="" className='logo'>✌🏽 Murtaza Abidi</a>
-        <div className="navigation-items">
-          <a href="" className="item">About</a>
-          <a href="" className="item">Projects</a>
-        </div>
-      </nav>
-
       <section>
-        <div className="hero">
-          <h1>{"Hi, I'm Murtaza."}</h1>
-          <h2>{"I'm a product designer at"} <a href="https://transferwise.com" className="highlight">TransferWise</a>. {"I design things to help solve problems for people. Sometimes I write about things I care about. And I shout about things on twitter too"}.</h2>
+        <div className="hero-container">
+          <img src={arrow1} className="arrow arrow1"></img>
+          <img src={arrow2} className="arrow arrow2"></img>
+          <img src={arrow1} className="arrow arrow3"></img>
+          <img src={arrow2} className="arrow arrow4"></img>
+          <img src={arrow1} className="arrow arrow5"></img>
+          <img src={arrow2} className="arrow arrow6"></img>
+          <div className="hero">
+            <h1>Hi, {"I'm Murtaza 👋🏽"}</h1>
+            <h2>{"I’m a product designer at"} <a href="https://transferwise.com" className="highlight">TransferWise</a>. {"I design things to help solve problems for people. Sometimes I"} <a href="https://medium.com/@murtz_abidi" className="highlight">write </a>{"about things I care about. And I shout about things on "}<a href="https://twitter.com/murtz_abidi" className="highlight">twitter</a>.</h2>
+          </div>
         </div>
       </section>
+      <section className="about-container">
+        <div className="about">
+          <div className="description">
+            <h3>About me</h3>
+            <p>
+              {"I've been designing things for the internet for almost 8 years. It started with "} <a href="https://casserolesclub.com" className="highlight2">Casserole Club</a> {"at FutureGov. Since then I've worked in agencies, corporates and startups learning a lot along the way."}
+            </p>
+            <br/>
+              <p>
+                {"I'm passionate about representation and started"} <a href="https://muslamicmakers.com" className="highlight2">Muslamic Makers</a> {"to help my community."}
+              </p>
 
-      <footer>
-        <a href="https://twitter.com/murtz_abidi">Twitter</a>
-      </footer>
+              <br/>
+              <p>{"Here's where you can find my"} <a href="https://drive.google.com/drive/folders/0ByFB_YO-8JPINEM2MHJfbWpHVm8" className="highlight2">CV and Portfolio</a></p>
+            <br/>
+            <p>
+              {"You can reach me by"} <a href="mailto://murtaza.s.abidi@gmail.com" className="highlight2">email</a> or <a href="https://twitter.com/murtz_abidi" className="highlight2">@murtz_abidi</a>.
+            </p>
+
+
+
+            </div>
+            <div className="photo">
+              <img src={murtaza}></img>
+            </div>
+        </div>
+      </section>
     </div>
   );
 }
