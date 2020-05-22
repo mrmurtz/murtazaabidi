a934bb9b0ce29387167412243c80e759a74e69a8
mrmurtz <murtaza.s.abidi@gmail.com>
2020-05-22 18:33:58 +0100

    Updates

---
+++ precache-manifest.507a42fa2a55b3733be4cc2398b9a9bd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977a16a63ff44432620247f68507ffa1",
+    "url": "/murtazaabidi/index.html"
+  },
+  {
+    "revision": "4a6ee9f2aa988ae53ba3",
+    "url": "/murtazaabidi/static/css/main.1febc7c0.chunk.css"
+  },
+  {
+    "revision": "9bd34cb822fa64f027b9",
+    "url": "/murtazaabidi/static/js/2.ceba51e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/murtazaabidi/static/js/2.ceba51e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a6ee9f2aa988ae53ba3",
+    "url": "/murtazaabidi/static/js/main.f4a6d57b.chunk.js"
+  },
+  {
+    "revision": "7abe2c97888a4835503c",
+    "url": "/murtazaabidi/static/js/runtime-main.ed6bc589.js"
+  },
+  {
+    "revision": "14f994f95a3b80152e72aadb37eb58b8",
+    "url": "/murtazaabidi/static/media/arrow1.14f994f9.svg"
+  },
+  {
+    "revision": "cea680b25fc8008b81a941cbc91fc5aa",
+    "url": "/murtazaabidi/static/media/arrow2.cea680b2.svg"
+  },
+  {
+    "revision": "461e79e6d6ae95b6a53e8193e122756e",
+    "url": "/murtazaabidi/static/media/logo.461e79e6.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/murtazaabidi/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0553b4b3a084d94f7006c0196b14aacc",
+    "url": "/murtazaabidi/static/media/murtaza.0553b4b3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ef711f9c1bdd784894c9b6e88016b9c.js"
+  "/murtazaabidi/precache-manifest.507a42fa2a55b3733be4cc2398b9a9bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/murtazaabidi/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
